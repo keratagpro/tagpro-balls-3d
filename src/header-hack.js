7a980e4d2ad600a5a30f08591ea7fda03561d73f
Kera <keratagpro@gmail.com>
2015-10-02 02:59:41 +0300

    Working PIXI integration. Adding Userscript downloadUrl and updateUrl attributes for automatic updates.

---
+++ src/header-hack.js
@@ -0,0 +1,2 @@
+// HACK: browserify-shim tries to find libraries from window.### and global.###
+window.THREE = window.THREE || THREE;
--- src/index.js
+++ src/index.js
@@ -1,172 +1,74 @@
+import THREE from 'three';
 import after from './lib/after_hook';
 import createBall from './lib/ball';
 
-var w = 1280;
-var h = 800;
-var aspect = w / h;
-var zoom = 1;
+THREE.ImageUtils.crossOrigin = '';
+
+const TILE_SIZE = 40;
+const TILES_HORIZONTAL = 10;
+const TILES_VERTICAL = 10;
+
+const CANVAS_WIDTH = TILE_SIZE * TILES_HORIZONTAL;
+const CANVAS_HEIGHT = TILE_SIZE * TILES_VERTICAL;
 
 var velocityCoefficient = 0.1;
 var rotationCoefficient = 1.0;
+
 var vecY = new THREE.Vector3(1, 0, 0);
 var vecX = new THREE.Vector3(0, -1, 0);
 var vecZ = new THREE.Vector3(0, 0, 1);
 
-GM_addStyle("#canvas3d { display: block; pointer-events: none; position: absolute; }");
+var renderer3d = createRenderer();
+var camera3d = createCamera();
+var scene3d = createScene();
 
-tagpro.ready(function() {
-	var camera = createCamera();
-	tagpro.threeCamera = camera;
+var container = new THREE.Object3D();
+container.position.x = -CANVAS_WIDTH/2;
+container.position.y = -CANVAS_HEIGHT/2;
+scene3d.add(container);
 
-	var tr = tagpro.renderer;
+var baseTexture3d = new PIXI.BaseTexture(renderer3d.domElement);
 
+function createRenderer() {
 	var renderer = new THREE.WebGLRenderer({
 		alpha: true,
 		antialias: true
 	});
 
-	renderer.setSize(w, h);
-	document.body.appendChild(renderer.domElement);
-	renderer.domElement.id = 'canvas3d';
+	renderer.setSize(CANVAS_WIDTH, CANVAS_HEIGHT);
 
-	var scene = new THREE.Scene();
-
-	addLights(scene);
-
-	THREE.ImageUtils.crossOrigin = '';
-
-	var textureEarth = THREE.ImageUtils.loadTexture(
-		'textures/planets/earthmap1k.jpg',
-		undefined,
-		render);
-	textureEarth.anisotropy = renderer.getMaxAnisotropy();
-	textureEarth.minFilter = THREE.LinearFilter;
-
-	var textureMars = THREE.ImageUtils.loadTexture(
-		'textures/planets/marsmap1k.jpg',
-		undefined,
-		render);
-	textureMars.anisotropy = renderer.getMaxAnisotropy();
-	textureMars.minFilter = THREE.LinearFilter;
-
-	// var earth = createBall(textureEarth);
-	// earth.position.x = 500;
-	// earth.position.y = 300;
-	// scene.add(earth);
-
-	// var mars = createBall(textureMars);
-	// mars.position.x = -500;
-	// mars.position.y = -300;
-	// scene.add(mars);
-
-	// var geometry = new THREE.PlaneGeometry( 32 * 40, 20 * 40, 32, 20 );
-	// var material = new THREE.MeshBasicMaterial( {color: 0xcccccc, side: THREE.DoubleSide, wireframe: true } );
-	// var plane = new THREE.Mesh( geometry, material );
-	// geometry.applyMatrix( new THREE.Matrix4().makeTranslation( -16 * 40, 10 * 40, 0 ) );
-	// scene.add( plane );
-
-	after(tr, 'createPlayerSprite', function(player) {
-		console.log('create', player);
-
-		player.lastAngle = player.angle; // initialize lastAngle
-
-		var texture = player.team === 2 ? textureEarth : textureMars;
-		player.sphere = createBall(texture);
-
-		scene.add(player.sphere);
-	});
-
-	after(tr, 'updatePlayerSpritePosition', function(player) {
-		player.sphere.material.visible = !player.dead;
-
-		player.sphere.position.x = -player.x;
-		player.sphere.position.y = player.y;
-
-		rotateAroundWorldAxis(player.sphere, vecX, player.lx * velocityCoefficient);
-		rotateAroundWorldAxis(player.sphere, vecY, -player.ly * velocityCoefficient);
-
-		var theta = player.angle - player.lastAngle;
-		player.lastAngle = player.angle;
-
-		rotateAroundWorldAxis(player.sphere, vecZ, -theta * rotationCoefficient);
-	});
-
-	after(tr, 'updatePlayerColor', function(player) {
-		var color = player.team == 1 ? 'red' : 'blue';
-		var tileId = color + 'ball';
-
-
-	});
-
-	after(tagpro.chat, 'resize', function() {
-		var cnv = tr.canvas;
-		renderer.setSize(cnv.width, cnv.height);
-	});
-
-	var origCameraPosition = tr.updateCameraPosition;
-	tr.updateCameraPosition = function(player) {
-		origCameraPosition(player);
-
-		setCameraPosition(camera, player);
-
-		if (tagpro.zoom !== zoom) {
-			zoom = tagpro.zoom;
-			camera.zoom = zoom;
-			camera.updateProjectionMatrix();
-		}
-	};
-
-	var origRender = tr.render;
-	tr.render = function(stage) {
-		origRender(stage);
-
-		render();
-	}
-
-	var origDestroyPlayer = tr.destroyPlayer;
-	tr.destroyPlayer = function(player) {
-		scene.remove(player.sphere);
-
-		origDestroyPlayer(player);
-	};
-
-	var rotWorldMatrix;
-
-	// Rotate an object around an arbitrary axis in world space       
-	function rotateAroundWorldAxis(object, axis, radians) {
-		rotWorldMatrix = new THREE.Matrix4();
-		rotWorldMatrix.makeRotationAxis(axis.normalize(), radians);
-		rotWorldMatrix.multiply(object.matrix); // pre-multiply
-		object.matrix = rotWorldMatrix;
-		object.rotation.setFromRotationMatrix(object.matrix);
-	}
-
-	function render() {
-		renderer.render(scene, camera);
-	};
-
-	render();
-});
+	return renderer;
+}
 
 function createCamera() {
-	var tiles = 20 / 2; // vertical tile count / 2
-	var d = tiles * (40 / zoom); // tile size
 	var camera = new THREE.OrthographicCamera(
-		-d * aspect, d * aspect,
-		d, -d,
+		-CANVAS_WIDTH/2, CANVAS_WIDTH/2,
+		-CANVAS_HEIGHT/2, CANVAS_HEIGHT/2,
 		1, 1000);
+
 	camera.position.z = 900;
-	camera.up = new THREE.Vector3(0, -1, 0);
+
+	camera.up = new THREE.Vector3(0, 1, 0);
 	camera.lookAt(new THREE.Vector3(0, 0, 0));
-	camera.updateProjectionMatrix();
 
 	return camera;
 }
 
-function setCameraPosition(camera, player) {
-	camera.position.x = -player.x;
-	camera.position.y = player.y;
-	camera.lookAt(player.sphere.position);
+function createScene() {
+	var scene = new THREE.Scene();
+
+	addLights(scene);
+
+	return scene;
+}
+
+function getSpherePositionForPlayer(player) {
+	var id = player.id - 1; // I hope tagpro.players is a number starting from 1.
+
+	return {
+		x: (id % TILES_HORIZONTAL) * TILE_SIZE,
+		y: ~~(id / TILES_HORIZONTAL) * TILE_SIZE // ~~ is basically Math.floor
+	};
 }
 
 function addLights(scene) {
@@ -177,3 +79,85 @@ function addLights(scene) {
 	light.position.set(500, -500, 500);
 	scene.add(light);
 }
+
+function getSphereTextureForPlayer(player, callback) {
+	var texturePath = (player.team === 1) ?
+		'http://jeromeetienne.github.io/threex.planets/images/marsmap1k.jpg' : 
+		'http://jeromeetienne.github.io/threex.planets/images/earthmap1k.jpg';
+
+	var texture = THREE.ImageUtils.loadTexture(texturePath, undefined, callback);
+	texture.anisotropy = renderer3d.getMaxAnisotropy();
+	texture.minFilter = THREE.LinearFilter;
+
+	return texture;
+}
+
+function render3d() {
+	renderer3d.render(scene3d, camera3d);
+}
+
+function createSphereForPlayer(player) {
+	var texture = getSphereTextureForPlayer(player, render3d);
+	return createBall(texture);
+}
+
+function addSphereToScene(scene, sphere, position) {
+	sphere.position.x = position.x + TILE_SIZE / 2;
+	sphere.position.y = position.y + TILE_SIZE / 2;
+
+	scene.add(sphere);
+}
+
+// Rotate an object around an arbitrary axis in world space       
+function rotateAroundWorldAxis(object, axis, radians) {
+	var rotWorldMatrix = new THREE.Matrix4();
+	rotWorldMatrix.makeRotationAxis(axis.normalize(), radians);
+	rotWorldMatrix.multiply(object.matrix); // pre-multiply
+	object.matrix = rotWorldMatrix;
+	object.rotation.setFromRotationMatrix(object.matrix);
+}
+
+tagpro.ready(function() {
+	var tr = tagpro.renderer;
+
+	after(tr, 'createBallSprite', function(player) {
+		var sphere = createSphereForPlayer(player);
+		player.sphere = sphere;
+
+		var position = getSpherePositionForPlayer(player);
+		addSphereToScene(container, sphere, position);
+
+		var frame = new PIXI.Rectangle(position.x, position.y, TILE_SIZE, TILE_SIZE);
+		var texture = new PIXI.Texture(baseTexture3d, frame);
+		player.sprites.actualBall.setTexture(texture);
+		baseTexture3d.dirty();
+
+		player.lastAngle = player.angle; // initialize lastAngle
+	});
+
+	after(tr, 'updatePlayerSpritePosition', function(player) {
+		// player.sphere.material.visible = !player.dead;
+
+		rotateAroundWorldAxis(player.sphere, vecX, player.lx * velocityCoefficient);
+		rotateAroundWorldAxis(player.sphere, vecY, player.ly * velocityCoefficient);
+
+		var theta = player.angle - player.lastAngle;
+		player.lastAngle = player.angle;
+
+		rotateAroundWorldAxis(player.sphere, vecZ, theta * rotationCoefficient);
+
+		baseTexture3d.dirty();
+	});
+
+	after(tr, 'render', function() {
+		render3d();
+	});
+
+	after(tr, 'destroyPlayer', function(player) {
+		scene.remove(player.sphere);
+
+		delete player.sphere;
+	})
+
+	render3d();
+});
