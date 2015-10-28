3a9dcf89791a55def09c791ef40c86da8b9a265b
Kera <keratagpro@gmail.com>
2015-10-28 14:35:51 +0200

    add version to gh-pages commit messages.

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -1,9 +1,12 @@
 import ghPages from 'gh-pages';
 import gitConfig from 'git-config';
 
+import project from '../package.json';
+
 export default function deploy(done) {
 	ghPages.publish('./build', {
 		user: gitConfig.sync('.git/config').user,
-		clone: '.publish'
+		clone: '.publish',
+		message: `Update to ${project.version}`
 	}, done);
 }
